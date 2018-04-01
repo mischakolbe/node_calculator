5870a60b0c41ea7fdcf52705a319c4a755684a12
Mischa Kolbe <mischakolbe@gmail.com>
2018-03-31 18:58:23 -0700

    Made metadataVar tests more generic, MetadataVar of MetadataVar is now of same baseclass

--- test/test_metadata_variables.py
+++ test/test_metadata_variables.py
@@ -3,6 +3,7 @@ Unit tests for metadata_variables.py
 """
 import unittest
 
+# Make sure relative paths don't mess up the import procedure, if script is run directly
 if __name__ == "__main__":
     import sys
     from os import path
@@ -12,124 +13,100 @@ else:
     from .. import metadata_variables
 
 
-class TestCore(unittest.TestCase):
+test_variables = [
+    True,  # Boolean
+    1,  # Integer
+    1.0,  # Float
+    "one",  # String
+    (1, 2, 3),  # Tuple
+    set([1, 2, 3]),  # Set
+    [1, 2, 3],  # List
+    {"one": 1, "two": 2}  # Dictionary
+]
 
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # TEST FOR CORRECT VALUE AND METADATA FOR EACH TYPE
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-    def test_bool(self):
-        variable = metadata_variables.var(True, "metadata")
-        self.assertEqual(variable, True)
+def _test_type(value):
+    """
+    Basic tests that a given value is correctly set up; correct value, type, metadata, ...
+    """
+    value_type = type(value)
+
+    def test(self):
+        variable = metadata_variables.var(value, "metadata")
+        self.assertEqual(variable, value)
         self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "BoolMetadataVar")
-        self.assertNotEqual(variable.__class__.__bases__[0], bool)
-        self.assertEqual(variable.basetype, int)
-        self.assertEqual(variable.__class__.__bases__[0], int)
+        self.assertEqual(
+            variable.__class__.__name__,
+            "{}MetadataVar".format(value_type.__name__.capitalize())
+        )
 
-    def test_int(self):
-        variable = metadata_variables.var(1, "metadata")
-        self.assertEqual(variable, 1)
-        self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "IntMetadataVar")
-        self.assertEqual(variable.basetype, int)
-        self.assertEqual(variable.__class__.__bases__[0], int)
+        # bool can not be a base type. Tests must reflect that! Redirecting bool to int!
+        if value_type is bool:
+            bool_proof_type = int
+        else:
+            bool_proof_type = value_type
 
-    def test_float(self):
-        variable = metadata_variables.var(1.0, "metadata")
-        self.assertEqual(variable, 1.0)
-        self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "FloatMetadataVar")
-        self.assertEqual(variable.basetype, float)
-        self.assertEqual(variable.__class__.__bases__[0], float)
+        self.assertEqual(variable.basetype, bool_proof_type)
+        self.assertEqual(variable.__class__.__bases__[0], bool_proof_type)
+    return test
 
-    def test_str(self):
-        variable = metadata_variables.var("one", "metadata")
-        self.assertEqual(variable, "one")
-        self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "StrMetadataVar")
-        self.assertEqual(variable.basetype, str)
-        self.assertEqual(variable.__class__.__bases__[0], str)
 
-    def test_tuple(self):
-        variable = metadata_variables.var((1, 2, 3), "metadata")
-        self.assertEqual(variable, (1, 2, 3))
-        self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "TupleMetadataVar")
-        self.assertEqual(variable.basetype, tuple)
-        self.assertEqual(variable.__class__.__bases__[0], tuple)
+def _test_type_singleton(value):
+    """
+    The instance for every given type should be from a singleton class!
+    """
 
-    def test_set(self):
-        variable = metadata_variables.var(set([1, 2, 3]), "metadata")
-        self.assertEqual(variable, set([1, 2, 3]))
-        self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "SetMetadataVar")
-        self.assertEqual(variable.basetype, set)
-        self.assertEqual(variable.__class__.__bases__[0], set)
-
-    def test_list(self):
-        variable = metadata_variables.var([1, 2, 3], "metadata")
-        self.assertEqual(variable, [1, 2, 3])
-        self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "ListMetadataVar")
-        self.assertEqual(variable.basetype, list)
-        self.assertEqual(variable.__class__.__bases__[0], list)
-
-    def test_dict(self):
-        variable = metadata_variables.var({"one": 1, "two": 2}, "metadata")
-        self.assertEqual(variable, {"one": 1, "two": 2})
-        self.assertEqual(variable.metadata, "metadata")
-        self.assertEqual(variable.__class__.__name__, "DictMetadataVar")
-        self.assertEqual(variable.basetype, dict)
-        self.assertEqual(variable.__class__.__bases__[0], dict)
-
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # TEST FOR CLASS SINGLETONS FOR EACH TYPE
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-    def test_bool_singleton(self):
-        variable_a = metadata_variables.var(True, "metadata_a")
-        variable_b = metadata_variables.var(False, "metadata_b")
+    def test(self):
+        variable_a = metadata_variables.var(value, "metadata_a")
+        variable_b = metadata_variables.var(value, "metadata_b")
         self.assertIs(variable_a.basetype, variable_b.basetype)
+    return test
 
-    def test_int_singleton(self):
-        variable_a = metadata_variables.var(1, "metadata_a")
-        variable_b = metadata_variables.var(2, "metadata_b")
-        self.assertIs(variable_a.basetype, variable_b.basetype)
 
-    def test_float_singleton(self):
-        variable_a = metadata_variables.var(1.0, "metadata_a")
-        variable_b = metadata_variables.var(2.0, "metadata_b")
-        self.assertIs(variable_a.basetype, variable_b.basetype)
+def _test_subclass_proof(value):
+    """
+    Making a variable of an existing MetadataVar should result in the same
+    baseclass-type, NOT a MetadataVar with basetype MetadataVar!
+    """
 
-    def test_str_singleton(self):
-        variable_a = metadata_variables.var("one", "metadata_a")
-        variable_b = metadata_variables.var("two", "metadata_b")
-        self.assertIs(variable_a.basetype, variable_b.basetype)
+    def test(self):
+        # bool can not be a base type. Tests must reflect that! Redirecting bool to int!
+        value_type = type(value)
+        if value_type is bool:
+            value_type = int
 
-    def test_tuple_singleton(self):
-        variable_a = metadata_variables.var((1, 2), "metadata_a")
-        variable_b = metadata_variables.var((3, 4), "metadata_b")
-        self.assertIs(variable_a.basetype, variable_b.basetype)
+        variable_a = metadata_variables.var(value, "metadata_a")
+        variable_b = metadata_variables.var(variable_a, "metadata_b")
 
-    def test_set_singleton(self):
-        variable_a = metadata_variables.var(set([1, 2]), "metadata_a")
-        variable_b = metadata_variables.var(set([3, 4]), "metadata_b")
-        self.assertIs(variable_a.basetype, variable_b.basetype)
+        self.assertIs(variable_a.basetype, value_type)
+        self.assertIs(variable_b.basetype, value_type)
+    return test
 
-    def test_list_singleton(self):
-        variable_a = metadata_variables.var([1, 2], "metadata_a")
-        variable_b = metadata_variables.var([3, 4], "metadata_b")
-        self.assertIs(variable_a.basetype, variable_b.basetype)
 
-    def test_dict_singleton(self):
-        variable_a = metadata_variables.var({"one": 1}, "metadata_a")
-        variable_b = metadata_variables.var({"two": 2}, "metadata_b")
-        self.assertIs(variable_a.basetype, variable_b.basetype)
+class TestMetadataVariablesMeta(type):
 
-    # def test_dict(self):
-    #     with self.assertRaises(NotImplementedError):
-    #         metadata_variables.var({"a": 1, "b": 2})
+    def __new__(_mcs, _name, _bases, _dict):
+        # Add tests for each given variable(-type)
+        for variable in test_variables:
+            type_nice_name = type(variable).__name__
+
+            # Add basic tests
+            type_test_name = "test_{}".format(type_nice_name)
+            _dict[type_test_name] = _test_type(variable)
+
+            # Add singleton test
+            type_singleton_test_name = "test_{}_singleton".format(type_nice_name)
+            _dict[type_singleton_test_name] = _test_type_singleton(variable)
+
+            # Add test to make sure MetadataVar of MetadataVar uses correct baseclass
+            type_subclass_test_name = "test_{}_subclass".format(type_nice_name)
+            _dict[type_subclass_test_name] = _test_subclass_proof(variable)
+
+        return type.__new__(_mcs, _name, _bases, _dict)
+
+
+class TestMetadataVariables(unittest.TestCase):
+    __metaclass__ = TestMetadataVariablesMeta
 
 
 if __name__ == '__main__':
