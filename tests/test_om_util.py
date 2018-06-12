05d49deb2071628f8a2eea0e3f73b170d13990c5
Mischa Kolbe <mischakolbe@gmail.com>
2018-06-12 01:26:13 -0700

    test_om_util: Added all currently necessary tests for om_util.py

--- tests/test_om_util.py
+++ tests/test_om_util.py
@@ -9,6 +9,8 @@ Unit tests for noca.om_util
 import unittest
 
 # Third party imports
+from maya import cmds
+from maya.api import OpenMaya
 
 # Local imports
 from cmt.test import TestCase
@@ -45,6 +47,45 @@ test_plug_strings = {
 
 class TestTracerClass(TestCase):
 
+    def setUp(self):
+        self.node_name = "testNode"
+        self.node_alt_name = "testAltName"
+        self.group_name = "testGrp"
+        self.instance_name = "testInstance"
+        self.operator_name = "testPlusMinusAverage"
+
+        # Create regular polyCube transform & shape node
+        self.test_node = cmds.polyCube(name=self.node_name, constructionHistory=False)[0]
+
+        # Add a second shape under the same transform node
+        shape_duplicate = cmds.duplicate(self.test_node)[0]
+        cmds.parent(
+            cmds.listRelatives(shape_duplicate, shapes=True)[0],
+            self.test_node,
+            relative=True,
+            shape=True,
+        )
+        cmds.delete(shape_duplicate)
+        self.test_shapes = cmds.listRelatives(self.test_node, shapes=True)
+
+        # Parent the polyCube node under a group
+        self.test_group = cmds.group(empty=True, name=self.group_name)
+        cmds.parent(self.test_node, self.test_group)
+
+        # Instantiate the polyCube
+        self.test_instance = cmds.instance(self.test_node, name=self.instance_name)[0]
+
+        # Create an operator node used for calculations
+        self.test_operator = cmds.createNode("plusMinusAverage", name=self.operator_name)
+        cmds.connectAttr(
+            "{}.t".format(self.node_name),
+            "{}.input3D[0]".format(self.operator_name)
+        )
+        cmds.connectAttr(
+            "{}.s".format(self.node_name),
+            "{}.input3D[3]".format(self.operator_name)
+        )
+
     def test_split_plug_string(self):
         """ Test plug strings are correctly split into their components """
 
@@ -58,45 +99,148 @@ class TestTracerClass(TestCase):
             attr_components = om_util.split_attr_string(attr_string)
             self.assertEqual(attr_components, desired_components[3:])
 
-    # def test_mobj(self):
-    #     """ Test mobj creation & retrieving """
-    #     om_util.get_name_of_mobj(mobj)
-    #     om_util.get_mobj(node)
-    #     om_util.get_long_name_of_mobj(mobj, full=True/False)
-    #     om_util.rename_mobj(mobj, name)
-    #     om_util.selected_nodes_in_scene_as_mobjs()
-    #     om_util.select_mobjs(mobjs)
+    def test_mobj(self):
+        """ Test mobj creation & retrieving """
 
-    # def test_mdag_path(self):
-    #     """ """
-    #     om_util.get_mdag_path(mobj)
+        # Test MObject creation
+        test_node_mobj = om_util.get_mobj(self.test_node)
+        self.assertEqual(type(test_node_mobj), OpenMaya.MObject)
+        self.assertEqual(type(om_util.get_mobj(test_node_mobj)), OpenMaya.MObject)
 
-    # def test_shape(self):
-    #     """ """
-    #     om_util.get_shape_mobjs(mobj)
+        # Test name retrieving of MObjects
+        self.assertEqual(om_util.get_name_of_mobj(test_node_mobj), self.node_name)
+        self.assertEqual(
+            om_util.get_long_name_of_mobj(test_node_mobj, full=False),
+            self.node_name
+        )
+        self.assertEqual(
+            om_util.get_long_name_of_mobj(test_node_mobj, full=True),
+            "|{}|{}".format(self.group_name, self.node_name)
+        )
 
-    # def test_node(self):
-    #     """ """
-    #     om_util.is_instanced(node)
-    #     om_util.get_node_type(node, api_type=False)
-    #     om_util.get_all_mobjs_of_type(dependency_node_type)
-    #     om_util.get_mfn_dag_node(node)
-    #     om_util.get_parents(node)
-    #     om_util.get_parent(node)
+        # Test renaming of MObjects
+        om_util.rename_mobj(test_node_mobj, self.node_alt_name)
+        self.assertEqual(om_util.get_name_of_mobj(test_node_mobj), self.node_alt_name)
+        om_util.rename_mobj(test_node_mobj, self.node_name)
 
-    # def test_plug(self):
-    #     """ """
-    #     om_util.set_mobj_attribute(mobj, attr, value)
-    #     om_util.get_attr_of_mobj(mobj, attr)
+        # Test MObject selection and retrieving of selection
+        om_util.select_mobjs(test_node_mobj)
+        self.assertEqual(cmds.ls(selection=True), [self.node_name])
+        selected_mobjs = om_util.selected_nodes_in_scene_as_mobjs()
+        self.assertEqual(len(selected_mobjs), 1)
+        self.assertEqual(om_util.get_name_of_mobj(selected_mobjs[0]), self.node_name)
 
-    #     om_util.get_mplug_of_mobj(mobj, attr)
-    #     om_util.is_valid_mplug(mplug)
-    #     om_util.get_parent_plug(mplug)
-    #     om_util.get_child_mplug(mplug, child)
-    #     om_util.get_child_mplugs(mplug)
-    #     om_util.get_array_plug_elements(mplug)
+    def test_mdag_path(self):
+        """ Test mdag path creation & retrieving """
 
-    #     om_util.get_array_plug_by_physical_index(mplug, index)
-    #     om_util.get_array_plug_by_logical_index(mplug, index)
-    #     om_util.get_mplug_of_node_and_attr(node, attr)
-    #     om_util.get_mplug_of_plug(plug)
+        test_node_mobj = om_util.get_mobj(self.test_node)
+        mdag_path = om_util.get_mdag_path(test_node_mobj)
+        self.assertEqual(type(mdag_path), OpenMaya.MDagPath)
+        self.assertEqual(str(mdag_path), self.node_name)
+
+    def test_shape(self):
+        """ Test shape creation & retrieving """
+
+        test_node_mobj = om_util.get_mobj(self.test_node)
+        shape_mobjs = om_util.get_shape_mobjs(test_node_mobj)
+        self.assertEqual(len(shape_mobjs), 2)
+        shape_mobj_name = om_util.get_name_of_mobj(shape_mobjs[0])
+        self.assertEqual(shape_mobj_name, "{}Shape".format(self.node_name))
+
+    def test_node(self):
+        """ Test node creation & retrieving """
+
+        # Test check for instantiated nodes
+        self.assertEqual(om_util.is_instanced(self.test_node), False)
+        self.assertEqual(om_util.is_instanced(self.test_instance), False)
+        self.assertEqual(
+            om_util.is_instanced(self.test_shapes[0]),
+            True
+        )
+
+        # Test type query
+        self.assertEqual(om_util.get_node_type(self.test_node, api_type=False), "transform")
+        self.assertEqual(om_util.get_node_type(self.test_node, api_type=True), "kTransform")
+
+        # Test relatives query
+        self.assertEqual(type(om_util.get_mfn_dag_node(self.test_node)), OpenMaya.MFnDagNode)
+        self.assertEqual(om_util.get_parent(self.test_shapes[0]), self.test_node)
+        _expected_parents = [self.test_node, self.test_group]
+        self.assertEqual(om_util.get_parents(self.test_shapes[0]), _expected_parents)
+
+    def test_plug(self):
+        """ """
+        test_node_mobj = om_util.get_mobj(self.test_node)
+        operator_mobj = om_util.get_mobj(self.test_operator)
+        transform_attr = "translateX"
+        parent_attr = "input3D"
+        array_attr = "{}[0]".format(parent_attr)
+        child_attr = "input3Dx"
+        test_value = 7
+
+        # Test getting an mplug from a plug
+        mplug = om_util.get_mplug_of_plug("{}.{}".format(self.test_operator, array_attr))
+        self.assertEqual(type(mplug), OpenMaya.MPlug)
+        self.assertEqual(str(mplug), "{}.{}".format(self.test_operator, array_attr))
+        mplug = om_util.get_mplug_of_node_and_attr(self.test_operator, array_attr)
+        self.assertEqual(type(mplug), OpenMaya.MPlug)
+        self.assertEqual(str(mplug), "{}.{}".format(self.test_operator, array_attr))
+
+        self.assertEqual(om_util.is_valid_mplug(mplug), True)
+
+        # Test attribute setting and getting
+        om_util.set_mobj_attribute(test_node_mobj, transform_attr, test_value)
+        queried_val = cmds.getAttr("{}.{}".format(self.test_node, transform_attr))
+        self.assertEqual(queried_val, test_value)
+        self.assertEqual(om_util.get_attr_of_mobj(test_node_mobj, transform_attr), queried_val)
+
+        # Test mplug query
+        parent_plug = om_util.get_mplug_of_mobj(operator_mobj, parent_attr)
+        self.assertEqual(type(parent_plug), OpenMaya.MPlug)
+        self.assertEqual(str(parent_plug), "{}.{}".format(self.test_operator, parent_attr))
+
+        # Test array attribute access
+        array_mplug_elements = om_util.get_array_mplug_elements(parent_plug)
+        array_mplug_elements_as_str = [str(var) for var in array_mplug_elements]
+        expected_array_plugs = [
+            "{}.{}[{}]".format(self.test_operator, parent_attr, var) for var in [0, 3]
+        ]
+        self.assertEqual(array_mplug_elements_as_str, expected_array_plugs)
+
+        # Test array mplug access via pysical index
+        mplug_by_index = str(om_util.get_array_mplug_by_physical_index(parent_plug, 0))
+        expected_plug = "{}.{}[0]".format(self.test_operator, parent_attr)
+        self.assertEqual(mplug_by_index, expected_plug)
+        mplug_by_index = str(om_util.get_array_mplug_by_physical_index(parent_plug, 1))
+        expected_plug = "{}.{}[3]".format(self.test_operator, parent_attr)
+        self.assertEqual(mplug_by_index, expected_plug)
+        mplug_by_index = om_util.get_array_mplug_by_physical_index(parent_plug, 2)
+        expected_plug = None
+        self.assertEqual(mplug_by_index, expected_plug)
+
+        # Test array mplug access via logical index
+        mplug_by_index = str(om_util.get_array_mplug_by_logical_index(parent_plug, 0))
+        expected_plug = "{}.{}[0]".format(self.test_operator, parent_attr)
+        self.assertEqual(mplug_by_index, expected_plug)
+        mplug_by_index = str(om_util.get_array_mplug_by_logical_index(parent_plug, 1))
+        expected_plug = "{}.{}[1]".format(self.test_operator, parent_attr)
+        self.assertEqual(mplug_by_index, expected_plug)
+        mplug_by_index = str(om_util.get_array_mplug_by_logical_index(parent_plug, 3))
+        expected_plug = "{}.{}[3]".format(self.test_operator, parent_attr)
+        self.assertEqual(mplug_by_index, expected_plug)
+
+        # Test child attribute access
+        child_mplug = om_util.get_child_mplug(mplug, child_attr)
+        expected_child_plug = "{}.{}.{}".format(self.test_operator, array_attr, child_attr)
+        self.assertEqual(str(child_mplug), expected_child_plug)
+
+        # Test parent attribute access
+        parent_mplug = om_util.get_parent_mplug(child_mplug)
+        self.assertEqual(str(parent_mplug), "{}.{}".format(self.test_operator, array_attr))
+
+        # Test children attributes access
+        child_mplugs = om_util.get_child_mplugs(parent_mplug)
+        expected_child_plugs = [
+            "{}.{}.input3D{}".format(self.test_operator, array_attr, var) for var in "xyz"
+        ]
+        self.assertEqual([str(var) for var in child_mplugs], expected_child_plugs)
